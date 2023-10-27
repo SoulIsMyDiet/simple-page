762528a505ed745ab9e00b1f8212ae43935d8ddb
Anzelm <marekd.90@gmail.com>
2023-10-27 17:43:12 +0200

    add component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import reactImg from './assets/react-core-concepts.png';
+import component from './assets/components.png';
+
 const keyWordExplainingReact = ['Nice', 'Innovative', 'Great'];
 const reactKeyWordLength = keyWordExplainingReact.length;
 function genReactKeyWord(reactKeyWordLength){
@@ -7,7 +10,7 @@ function Header() {
     const keyWord = keyWordExplainingReact[genReactKeyWord(reactKeyWordLength)];
     return (
         <header>
-            <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+            <img src={reactImg} alt="Stylized atom" />
             <h1>React Essentials</h1>
             <p>
                 {keyWord} React concepts you will need for almost any app you are
@@ -17,12 +20,36 @@ function Header() {
     )
 }
 
+function ReactComponnent(props){
+    return (
+        <li>
+            <img src={props.img} alt={props.alt}/>
+            <div>
+                <h3>{props.title}</h3>
+                <p>{props.description}</p>
+            </div>
+        </li>
+    )
+}
+
 function App() {
   return (
       <div>
           <Header/>
           <main>
               <h2>Time to get started!</h2>
+              <div>
+                  <section id="core-concepts">
+                      <h2>Core concepts</h2>
+                      <ul>
+                          <ReactComponnent title='Component' description='some description'
+                          img={component} alt='component'/>
+                          <ReactComponnent/>
+                          <ReactComponnent/>
+                          <ReactComponnent/>
+                      </ul>
+                  </section>
+              </div>
           </main>
       </div>
   );
