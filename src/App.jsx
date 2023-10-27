d262b2252b85a684c1428265700800bcb04d8e7a
Anzelm <marekd.90@gmail.com>
2023-10-27 16:03:20 +0200

    add component and randomize keyword about react

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
+const keyWordExplainingReact = ['Nice', 'Innovative', 'Great'];
+const reactKeyWordLength = keyWordExplainingReact.length;
+function genReactKeyWord(reactKeyWordLength){
+    return Math.floor(Math.random() * reactKeyWordLength);
+}
+function Header() {
+    const keyWord = keyWordExplainingReact[genReactKeyWord(reactKeyWordLength)];
+    return (
+        <header>
+            <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+            <h1>React Essentials</h1>
+            <p>
+                {keyWord} React concepts you will need for almost any app you are
+                going to build!
+            </p>
+        </header>
+    )
+}
+
 function App() {
   return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
+      <div>
+          <Header/>
+          <main>
+              <h2>Time to get started!</h2>
+          </main>
+      </div>
   );
 }
 
